a4671c618b079c305e4649f572ad5cff369b41c8
steveherrmannwm <steven.herrmannwm@gmail.com>
2017-03-29 02:44:17 -0600

    Resolved issue related to subtraction

--- expr.scala
+++ expr.scala
@@ -2,9 +2,6 @@ import sexp._
 
 sealed abstract class Exp
 case class Literal(v: SExp) extends Exp
-case class Add(lhs: Exp, rhs: Exp) extends Exp
-case class Subtract(lhs: Exp, rhs: Exp) extends Exp
-case class Multiply(lhs: Exp, rhs: Exp) extends Exp
 case class Call(funcName : Exp, args : List[Exp]) extends Exp
 case class Let(v : String,
                rhs: Exp,
@@ -12,18 +9,6 @@ case class Let(v : String,
 case class Ref(v : String) extends Exp
 case class Conditional(cond: Exp, true_case: Exp, false_case: Exp) extends Exp
 case class Lambda(args: List[String], body: Exp) extends Exp
-
-case class Equal(lhs: Exp, rhs: Exp) extends Exp
-case class LessThan(lhs: Exp, rhs: Exp) extends Exp
-case class LessThanEqual(lhs: Exp, rhs: Exp) extends Exp
-case class GreaterThan(lhs: Exp, rhs: Exp) extends Exp
-case class GreaterThanEqual(lhs: Exp, rhs: Exp) extends Exp
-
-case class Cons(head: Exp, tail: Exp) extends Exp
-case class Null(exp: Exp) extends Exp
-case class Car(exp: Exp) extends Exp
-case class Cdr(exp: Exp) extends Exp
-case class Pair(exp: Exp) extends Exp
 
 case class Def(name : String, arguments : List[String], body : Exp)
 case class Program(defs : List[Def], exp : Exp)
@@ -81,13 +66,8 @@ def parseExp(e: SExp) : Exp = {
       case STrue() => Literal(STrue())
       case SFalse() => Literal(SFalse())
       case SInt(v) => Literal(SInt(v))
+      case SSymbol("null") => Literal(SNil)
       case SList(SSymbol("quote"), exp) => Literal(exp)
-      case SList(SSymbol("+"), l, r) =>
-        Add(parseExp(l), parseExp(r))
-      case SList(SSymbol("-"), l, r) =>
-        Subtract(parseExp(l), parseExp(r))
-      case SList(SSymbol("*"), l, r) =>
-        Multiply(parseExp(l), parseExp(r))
       case SSymbol(id) => Ref(id)
       case SList(SSymbol("let"),
                  SList(SList(SSymbol(id), rhs)),
@@ -97,18 +77,6 @@ def parseExp(e: SExp) : Exp = {
         Lambda(parseArgs(args, List()), parseExp(body))
       case SList(SSymbol("if"), cond, t, f) =>
         Conditional(parseExp(cond), parseExp(t), parseExp(f))
-      case SList(SSymbol("equal?"), lhs, rhs) =>
-        Equal(parseExp(lhs), parseExp(rhs))
-     case SList(SSymbol("cons"), h, t) =>
-        Cons(parseExp(h), parseExp(t))
-      case SList(SSymbol("car"), exp) =>
-        Car(parseExp(exp))
-      case SList(SSymbol("cdr"), exp) =>
-        Cdr(parseExp(exp))
-      case SList(SSymbol("pair?"), exp) =>
-        Pair(parseExp(exp))
-      case SList(SSymbol("null?"), exp) =>
-        Null(parseExp(exp))
       // If we're dealing with a function, we may not know how many arguments we're dealing with, so use a special function
       case function => function match {
         case SNil => throw new IllegalArgumentException("Illegal function call")
@@ -162,37 +130,82 @@ def interpExp(e: Exp, env : Env) : SExp = {
               val newEnv = funcEnv.contents.get ++ argVals.toMap
               interpExp(body, newEnv)
             }
+            case Primitive("Add") => {
+              eargs.foldLeft(SInt(0))((a,b) =>
+                  (a,b) match {
+                    case (SInt(lhs), SInt(rhs)) =>
+                      SInt(lhs + rhs)
+                    case _ => throw new RuntimeException("Can only add numbers")
+                  })
+            }
+            case Primitive("Subtract") =>{
+              val hd::tail = eargs
+              val SInt(start) = hd
+              val SInt(toSubtract) = tail.foldLeft(SInt(0))((a,b) =>
+                  (a,b) match {
+                    case (SInt(lhs), SInt(rhs)) =>
+                      SInt(rhs + lhs)
+                    case _ => throw new RuntimeException("Can only subtract numbers")
+                  })
+              SInt(start - toSubtract)
+            }
+            case Primitive("Multiply") =>{
+              eargs.foldLeft(SInt(1))((a,b) =>
+                  (a,b) match {
+                    case (SInt(lhs), SInt(rhs)) =>
+                      SInt(lhs * rhs)
+                    case _ => throw new RuntimeException("Can only multiply numbers")
+                  })
+            }
+            case Primitive("Null") => {
+              // This function only accepts one argument, so assume that there is one
+              val List(nul) = eargs
+              nul match {
+                case SNil => STrue()
+                case _ => SFalse()
+              }
+            }
+            case Primitive("Pair") => {
+              val List(pair) = eargs
+              pair match {
+                case SList(a) => STrue()
+                case _ => SFalse()
+              }
+            }
+            case Primitive("cons") => {
+              val arg1 = eargs(0)
+              val arg2 = eargs(1)
+
+              SCons(arg1, arg2)
+            }
+            case Primitive("cdr") => {
+              val List(list) = eargs
+              list match {
+                case SCons(head, tail) => tail
+                case _ => throw new RuntimeException("Expect pair as the argument")
+              }
+            }
+            case Primitive("car") => {
+              val List(list) = eargs
+              list match {
+                case SCons(head, tail) => head
+                case _ => throw new RuntimeException("Expect pair as the argument")
+              }
+            }
+            case Primitive("Equals") => {
+              val arg1 = eargs(0)
+              val arg2 = eargs(1)
+              if (arg1 == arg2)
+                STrue()
+              else
+                SFalse()
+            }
           }
         }
         case Lambda(args, body) => {
           val envBox = new Box[Env]()
           envBox.contents = Some(env)
           Closure(Lambda(args, body), envBox)
-        }
-        case Add(l,r) => {
-          val lv = interpExp(l, env)
-          val rv = interpExp(r, env)
-          (lv, rv) match {
-            case (SInt(l), SInt(r)) => SInt(l + r)
-            case _ => throw new RuntimeException("An error occurred while adding")
-          }
-        }
-        case Subtract(l,r) => {
-          val lv = interpExp(l, env)
-          val rv = interpExp(r, env)
-
-          (lv, rv) match {
-            case (SInt(l), SInt(r)) => SInt(l-r)
-            case _ => throw new RuntimeException("An error occurred while subtracting")
-          }
-        }
-        case Multiply(l,r) => {
-          val lv = interpExp(l, env)
-          val rv = interpExp(r, env)
-          (lv, rv) match {
-            case (SInt(l), SInt(r)) => SInt(l * r)
-            case _ => throw new RuntimeException("An error occurred while multipling")
-          }
         }
         case Ref(id) => {
           env.get(id) match {
@@ -220,63 +233,19 @@ def interpExp(e: Exp, env : Env) : SExp = {
                 case _ => throw new RuntimeException("unable to parse conditional")
             }
         }
-        case Equal(l,r) => {
-          val lv = interpExp(l, env)
-          val rv = interpExp(r, env)
-
-          (lv, rv) match {
-            case (SInt(l), SInt(v)) =>
-              if(l == v)
-                STrue()
-              else
-                SFalse()
-            case (STrue(), STrue()) => STrue()
-            case (SFalse(), SFalse()) => STrue()
-            case _ => SFalse()
-        }
-      }
-      case Cons(l, r) => {
-        SCons(interpExp(l, env), interpExp(r, env))
-      }
-      case Car(exp) =>
-      {
-        val l = interpExp(exp, env)
-        l match {
-          case SNil => throw new RuntimeException("Structure given to car wasn't a pair")
-          case SCons(head, tail) => {
-            head
-          }
-        }
-      }
-      case Cdr(exp) => {
-        val l = interpExp(exp, env)
-        l match {
-          case SNil => throw new RuntimeException("structure given to Cdr wasn't a pair")
-          case SCons(head, tail) => tail
-        }
-      }
-      case Pair(exp) => {
-        val l = interpExp(exp, env)
-
-        l match {
-          case SList(a) => STrue()
-          case _ => SFalse()
-        }
-      }
-      case Null(exp) => {
-        val l = interpExp(exp, env)
-        l match {
-          case SNil => STrue()
-          case _ => SFalse()
-        }
-      }
     }
 }
 
 val initialEnv : Env = Map(
-  ("+" -> Primitive("+")),
-  ("-" -> Primitive("-")),
-  ("*" -> Primitive("*"))
+  ("+" -> Primitive("Add")),
+  ("-" -> Primitive("Subtract")),
+  ("*" -> Primitive("Multiply")),
+  ("cons" -> Primitive("cons")),
+  ("car" -> Primitive("car")),
+  ("cdr" -> Primitive("cdr")),
+  ("pair?" -> Primitive("Pair")),
+  ("null?" -> Primitive("Null")),
+  ("equal?" -> Primitive("Equals"))
   )
 
 def interpProgram(p : Program) : SExp = {
@@ -301,33 +270,20 @@ val arithEx1 = parseExp(parseSExp("(* (- 5 2) 1)"))
 val letEx1 = parseExp(parseSExp(
   """(let ((x 5))
        (+ x 7))"""))
-assert(interpExp(letEx1, Map()) == SInt(12))
+assert(interpExp(letEx1, initialEnv) == SInt(12))
 
 val letEx2 = parseExp(parseSExp(
   """(let ((x (+ 5 6)))
        (+ x 7))"""))
-assert(interpExp(letEx2, Map()) == SInt(18))
+assert(interpExp(letEx2, initialEnv) == SInt(18))
 
 val letEx3 = parseExp(parseSExp(
   """(let ((x (let ((y 1))
                  (+ y 2))))
         (+ x 3))"""))
-assert(interpExp(letEx3, Map()) == SInt(6))
-
+assert(interpExp(letEx3, initialEnv) == SInt(6))
 // Program parsing tests. We'll represent a program
 // as a sequence of defintions, followed by a main
-val progEx1 = parseProgram(parseSExp(
-"""
-((define (square n)
-   (* n n))
- (square 5))
-"""))
-(progEx1 ==
-  Program(List(
-    Def("square", List("n"), Multiply(Ref("n"), Ref("n")))
-  ),
-  Call(Literal(SSymbol("square")), List(Literal(SInt(5)))))
-)
 
 val progEx3 = parseProgram(parseSExp("""
  ((define (append l s)
@@ -336,53 +292,37 @@ val progEx3 = parseProgram(parseSExp("""
                   (cons (car l) (append (cdr l) s))))
                (append (quote (1 2 3)) (quote (4 5 6))))
   """))
-
-assert(evalExp(
+(evalExp(
   """
   (let ((two 2))
     (let ((double (lambda (n) (* n two))))
       (double 5)))
   """) == SInt(10))
 
+(evalProgram(
+  """
+  ((define (myf arg)
+     (+ arg 1))
+    (define (apply f)
+         (f 4))
+        (apply myf))
+  """
+) == SInt(5))
+
+assert(evalProgram(
+  """
+  ((define (foldLeft f l acc)
+     (if (null? l)
+            acc
+                 (foldLeft f (cdr l) (f (car l) acc))))
+              (foldLeft + (quote (1 2 3)) 0))
+  """) == SInt(6))
+
 // Returns the sum of the power sums of a given x and n from n to 0
 // Only works with positive integers for x and n
 // x must also be at least 1
-val testProg = parseProgram(parseSExp("""
- ((define (pow x exp)
-   (if (equal? exp 0)
-     1
-     (* x (pow x (- exp 1)))
-     ))
-  (define (sumOfPows a exp) (if (equal? a 0)
-    0
-    (+ (pow a exp) (sumOfPows (- a 1) exp))))
-  (define (sumsOfPows a exp) (if (equal? exp 1)
-    (sumOfPows a 1)
-    (+ (sumOfPows a exp) (sumsOfPows a (- exp 1)))))
-  (sumsOfPows 3 3)
-  )
-  """))
-assert(interpProgram(testProg) == SInt(56))
 
-
-
-val testProg2 = parseProgram(parseSExp("""
- ((define (pow x exp)
-   (if (equal? exp 0)
-     1
-     (* x (pow x (- exp 1)))
-     ))
-  (define (sumOfPows a exp) (if (equal? a 1)
-    1
-    (+ (pow a exp) (sumOfPows (- a 1) exp))))
-  (define (sumsOfPows a exp) (if (equal? exp 1)
-    (sumOfPows a 1)
-    (+ (sumOfPows a exp) (sumsOfPows a (- exp 1)))))
-  (sumsOfPows 5 4)
-  )
-  """))
-assert(interpProgram(testProg2) == SInt(1274))
-val testProg3 = parseProgram(parseSExp("""
+val testProg1 = parseProgram(parseSExp("""
  ((define (pow x exp)
    (if (equal? exp 0)
      1
@@ -397,5 +337,491 @@ val testProg3 = parseProgram(parseSExp("""
   (sumsOfPows 1 1)
   )
   """))
-assert(interpProgram(testProg3) == SInt(1))
+assert(interpProgram(testProg1) == SInt(1))
+val testProg2 = parseProgram(parseSExp("""
+ ((define (pow x exp)
+   (if (equal? exp 0)
+     1
+     (* x (pow x (- exp 1)))
+     ))
+  (define (sumOfPows a exp) (if (equal? a 1)
+    1
+    (+ (pow a exp) (sumOfPows (- a 1) exp))))
+  (define (sumsOfPows a exp) (if (equal? exp 1)
+    (sumOfPows a 1)
+    (+ (sumOfPows a exp) (sumsOfPows a (- exp 1)))))
+  (sumsOfPows 1 2)
+  )
+  """))
+assert(interpProgram(testProg2) == SInt(2))
+
+
+// FINAL TESTS
+// Math
+
+assert(
+  evalProgram(
+  """
+  (
+    (* (+ 1 2) 4)
+  )
+  """
+  )
+==
+SInt(12)
+)
+
+// Let
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((x (+ 1 2)))
+      (* x 4))
+  )
+  """
+  )
+==
+SInt(12)
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((x (let ((y 1))
+               (+ y 2))))
+      (* x 4))
+  )
+  """
+  )
+==
+SInt(12)
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((x 2))
+      (let ((y 4))
+        (let ((x 3))
+          (* x y))))
+  )
+  """
+  )
+==
+SInt(12)
+)
+
+// Lists
+
+assert(
+  evalProgram(
+  """
+  (
+    null
+  )
+  """
+  )
+==
+SNil
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((x null))
+      (null? x))
+  )
+  """
+  )
+==
+STrue()
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((x null))
+      (pair? x))
+  )
+  """
+  )
+==
+SFalse()
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((l (cons 1 2)))
+      (car l))
+  )
+  """
+  )
+==
+SInt(1)
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((l (cons 1 2)))
+      (cdr l))
+  )
+  """
+  )
+==
+SInt(2)
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((l (cons 1 2)))
+      (pair? l))
+  )
+  """
+  )
+==
+STrue()
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((l (cons 1 2)))
+      (null? l))
+  )
+  """
+  )
+==
+SFalse()
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (null? 5)
+  )
+  """
+  )
+==
+SFalse()
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (pair? 5)
+  )
+  """
+  )
+==
+SFalse()
+)
+
+// Conditionals
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((c #f))
+      (if c 5 6))
+  )
+  """
+  )
+==
+SInt(6)
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((c (cons 1 2)))
+      (if c 5 6))
+  )
+  """
+  )
+==
+SInt(5)
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((c #t))
+      (if c 5 6))
+  )
+  """
+  )
+==
+SInt(5)
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((v1 5))
+      (equal? v1 5))
+  )
+  """
+  )
+==
+STrue()
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((v1 5))
+      (equal? v1 6))
+  )
+  """
+  )
+==
+SFalse()
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (let ((v1 #f))
+      (equal? v1 #f))
+  )
+  """
+  )
+==
+STrue()
+)
+
+
+// Define
+
+assert(
+  evalProgram(
+  """
+  (
+    (define (f)
+      5)
+    (f)
+  )
+  """
+  )
+==
+SInt(5)
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (define (f arg)
+      (+ 1 arg))
+    (f 5)
+  )
+  """
+  )
+==
+SInt(6)
+)
+
+assert(
+  evalProgram(
+  """
+  (
+    (define (g) 1)
+    (define (f arg)
+      (+ (g) arg))
+    (f 5)
+  )
+  """
+  )
+==
+SInt(6)
+)
+
+// Checks that reference to variable by dynamic scope is an error;
+// you should implement lexical scope.
+assert(
+  try {
+    evalProgram(
+      """
+      (
+        (define (g) x)
+        (define (f x)
+          (g))
+        (f 5)
+      )
+    """)
+    false
+ } catch {
+   case e: Exception => true
+ }
+)
+
+// Lambda
+
+assert(evalProgram(
+  """
+  ((let ((double (lambda (n) (* n 2))))
+    (double 5)))
+  """) == SInt(10))
+
+assert(evalProgram(
+  """
+  ((let ((two 2))
+    (let ((double (lambda (n) (* n two))))
+      (double 5))))
+  """) == SInt(10))
+
+assert(evalProgram(
+  """
+  ((((lambda (x) (lambda (y) x))
+    5)
+   6))
+  """)
+  == SInt(5))
+
+assert(evalProgram(
+  """
+  ((let ((twice (lambda (f) (lambda (arg) (f (f arg))))))
+    ((twice (lambda (x) (* x 2))) 5)))
+  """)
+  == SInt(20))
+
+// Higher-order primitives
+
+assert(evalProgram(
+  """
+  ((let ((apply (lambda (f) (f 3 4))))
+     (cons (apply +)
+           (apply cons))))
+  """
+) == SCons(SInt(7), SCons(SInt(3), SInt(4))))
+
+assert(evalProgram(
+  """
+  ((define (foldLeft f l acc)
+   (if (null? l)
+     acc
+     (foldLeft f (cdr l) (f (car l) acc))))
+ (foldLeft + (quote (1 2 3)) 0))
+ """)
+ == SInt(6))
+
+// Real programs
+
+assert(
+  evalProgram("""
+  ((define (append l s)
+   (if (null? l)
+     s
+     (cons (car l) (append (cdr l) s))))
+   (append (quote (1 2 3)) (quote (4 5 6))))
+
+  """)
+  ==
+  SList(SInt(1), SInt(2), SInt(3), SInt(4), SInt(5), SInt(6))
+)
+
+assert(
+  evalProgram(
+    """
+    (
+    (define (even? n)
+      (if (equal? n 0)
+        #t
+        (odd? (- n 1))))
+    (define (odd? n)
+      (if (equal? n 0)
+        #f
+        (even? (- n 1))))
+    (even? 10)
+    )
+    """)
+  ==
+  STrue()
+)
+
+// If you're curious what this is, dig in here!
+// http://matt.might.net/articles/compiling-up-to-lambda-calculus/
+assert(evalProgram(
+  """
+  ((define (succ n) (+ n 1))
+   (define (natify church-numeral)
+     ((church-numeral succ) 0))
+   (natify ((lambda (f) (f (lambda (f) (lambda (z) (f (f (f (f (f z)))))))))
+      (((lambda (y) (lambda (F) (F (lambda (x) (((y y) F) x)))))
+        (lambda (y) (lambda (F) (F (lambda (x) (((y y) F) x))))))
+      (lambda (f)
+        (lambda (n)
+          ((((((lambda (n)
+            ((n (lambda (_) (lambda (t) (lambda (f) (f (lambda (void) void))))))
+              (lambda (t) (lambda (f) (t (lambda (void) void))))))
+            (((lambda (n)
+              (lambda (m)
+                ((m
+                  (lambda (n)
+                    (lambda (f)
+                      (lambda (z)
+                        (((n (lambda (g) (lambda (h) (h (g f)))))
+                          (lambda (u) z))
+                        (lambda (u) u))))))
+                      n)))
+                    n)
+                  (lambda (f) (lambda (z) z))))
+                (lambda (_)
+                  ((lambda (n)
+                    ((n (lambda (_) (lambda (t) (lambda (f) (f (lambda (void) void))))))
+                      (lambda (t) (lambda (f) (t (lambda (void) void))))))
+                    (((lambda (n)
+                      (lambda (m)
+                        ((m
+                          (lambda (n)
+                            (lambda (f)
+                              (lambda (z)
+                                (((n (lambda (g) (lambda (h) (h (g f)))))
+                                  (lambda (u) z))
+                                (lambda (u) u))))))
+                              n)))
+                            (lambda (f) (lambda (z) z)))
+                          n))))
+                        (lambda (_) (lambda (t) (lambda (f) (f (lambda (void) void))))))
+                      (lambda (_) (lambda (f) (lambda (z) (f z)))))
+                    (lambda (_)
+                      (((lambda (n) (lambda (m) (lambda (f) (lambda (z) ((m (n f)) z))))) n)
+                        (f
+                          (((lambda (n)
+                            (lambda (m)
+                              ((m
+                                (lambda (n)
+                                  (lambda (f)
+                                    (lambda (z)
+                                      (((n (lambda (g) (lambda (h) (h (g f)))))
+                                        (lambda (u) z))
+                                      (lambda (u) u))))))
+                                    n)))
+                                  n)
+                                (lambda (f) (lambda (z) (f z))))))))))))))
+                              """
+                            )
+                          == SInt(120))
+
 
